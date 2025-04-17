90c84c0ea8af71618ad9bbc19cea4e57d3251239
kdix-23-0240 <2312110240g@kindai.ac.jp>
2025-04-17 18:45:17 +0900

    TextManagerを実装

---
+++ Assets/SOLIDScene/Scripts/Text/TextManager.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SOLIDScene
+{
+    public class TextManager : MonoBehaviour
+    {
+        public Text timeText;
+        public Text scoreText;
+        private float timeCount = 0f;
+
+        private void Start()
+        {
+            timeText.text = "0";
+        }
+
+        private void Update()
+        {
+            if(GameSystem.IsGameOver)
+            {
+                scoreText.text = ((int)timeCount * 100).ToString();
+                return;
+            }
+
+            if (!GameSystem.IsGameOver)
+            {
+                timeCount += Time.deltaTime;
+                timeText.text = ((int)timeCount).ToString();
+            }
+        }
+
+        public void ResetTimeCount()
+        {
+            timeCount = 0f;
+        }
+    }
+}
