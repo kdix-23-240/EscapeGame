2f65969a98def17324662117c7c5902c7588b1b3
kdix-23-0240 <2312110240g@kindai.ac.jp>
2025-04-19 13:13:12 +0900

    Ansディレクトリにスクリプトのコピー追加

---
+++ Assets/SOLIDScene/Ans/Scripts/Meteorite/Meteorite.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+namespace SOLIDScene
+{
+    public class Meteorite : MonoBehaviour
+    {
+        private float posX;
+        private float posY;
+        private IMovable moveable;
+
+        void Start()
+        {
+            posX = this.gameObject.transform.position.x;
+            posY = this.gameObject.transform.position.y;
+
+            moveable = this.gameObject.GetComponent<IMovable>();
+        }
+
+        void Update()
+        {
+            if (GameSystem.IsGameOver)
+            {
+                return;
+            }
+
+            Debugger.Log("Meteorite Update called");
+
+            moveable.Move();
+
+            Debugger.Log("Meteorite Move called");
+        }
+
+        public void Reset()
+        {
+            this.gameObject.SetActive(true);
+            transform.position = new Vector3(posX, posY, 0);
+        }
+    }
+}
---
+++ Assets/SOLIDScene/Ans/Scripts/Move/PlayerMove.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+namespace SOLIDScene
+{
+    public class PlayerMove : MonoBehaviour, IMovable
+    {
+        public void Move()
+        {
+            if (-8 < this.gameObject.transform.position.x)
+            {
+                if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
+                {
+                    this.gameObject.transform.position += new Vector3(-2, 0, 0);
+                }
+            }
+
+            if (this.gameObject.transform.position.x < 8)
+            {
+                if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
+                {
+                    this.gameObject.transform.position += new Vector3(2, 0, 0);
+                }
+            }
+        }
+    }
+}
